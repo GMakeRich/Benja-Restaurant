974717466732f1b9fa71752152b39913c136496a
GMakeRich@gmail.com <GMakeRich@gmail.com>
2016-02-29 12:04:21 +0700

    Test Insert DB Finish

--- app/src/main/java/uih/chalermpol_k/benjarestaurant/MYOpenHelper.java
+++ app/src/main/java/uih/chalermpol_k/benjarestaurant/MYOpenHelper.java
@@ -5,39 +5,40 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 /**
- * Created by chalermpol on 29-Feb-16.
+ * Created by masterUNG on 2/29/16 AD.
  */
 public class MYOpenHelper extends SQLiteOpenHelper{
 
     //Explicit
-    public static final String database_name ="benja.db";
-    private static final int database_version =1;
+    public static final String database_name = "banja.db";
+    private static final int database_version = 1;
+
     private static final String create_table_user = "create table userTABLE (" +
             "_id integer primary key, " +
             "User text, " +
-            "Passwrod text, " +
-            "Name text, );";
+            "Password text, " +
+            "Name text);";
+
     private static final String create_table_food = "create table foodTABLE (" +
-            "_id integer primary key," +
-            "Food text," +
-            "Price text," +
+            "_id integer primary key, " +
+            "Food text, " +
+            "Price text, " +
             "Source text);";
 
+
     public MYOpenHelper(Context context) {
-        super(context, database_name,null,database_version);
+        super(context, database_name, null, database_version);
+    }   // Constructor
 
-
-    }   //Contructor
 
     @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL(create_table_food);
-        db.execSQL(create_table_user);
-
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(create_table_food);
+        sqLiteDatabase.execSQL(create_table_user);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
     }
 }   // Main Class
--- app/src/main/java/uih/chalermpol_k/benjarestaurant/MainActivity.java
+++ app/src/main/java/uih/chalermpol_k/benjarestaurant/MainActivity.java
@@ -5,9 +5,25 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Explicit
+    private MyManage myManage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Request Database
+        myManage = new MyManage(this);
+
+        //Test add Value
+        testAddValue();
+
+    }   // Main Method
+
+    private void testAddValue() {
+        myManage.addUser("testUser", "1234", "โดรามอน");
+        myManage.addFood("ไข่เจียว", "100", "urlFood");
     }
-}
+
+}   // Main Class
